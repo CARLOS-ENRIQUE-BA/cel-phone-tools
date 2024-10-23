8efc877723bb1fe1a21134f4a29c52c487c7a68b
CARLOS-ENRIQUE-BA <carlos_enbagui@hotmail.com>
2024-10-23 00:20:23 -0600

    Updating the homepage with personal data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,25 @@
-// pages/HomePage.js
+// src/pages/HomePage.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, Linking } from 'react-native';
 
 const HomePage = () => {
+  const openGitHubRepo = () => {
+    Linking.openURL('https://github.com/CARLOS-ENRIQUE-BA/cel-phone-tools');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Page</Text>
+      <Image
+        source={require('../../assets/logo.png')}  // Cambia el nombre del archivo por el nombre de tu imagen
+        style={styles.image}
+      />
+      <Text style={styles.title}>Universidad Politécnica de Chiapas</Text>
+      <Text style={styles.subtitle}>Ingeniería en Software</Text>
+      <Text style={styles.subtitle}>Carlos Enrique Barriga Aguilar</Text>
+      <Text style={styles.subtitle}>221188</Text>
+      <Text style={styles.subtitle}>Programación para moviles II</Text>
+      <Text style={styles.subtitle}>9-A</Text>
+      <Button title="Ver Repositorio en GitHub" onPress={openGitHubRepo} />
     </View>
   );
 };
@@ -15,9 +29,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 20,
   },
-  text: {
+  image: {
+    width: 200, // Ajusta el ancho según tus necesidades
+    height: 200, // Ajusta la altura según tus necesidades
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center', // Centra el texto
+  },
+  subtitle: {
     fontSize: 18,
+    marginBottom: 5,
+    textAlign: 'center', // Centra el texto
   },
 });
 
